e9d18f02e55b83d957696581c7d91ba45e333b9f
Pratham <pratham10600@gmail.com>
2025-08-29 08:09:36 +0530

    indepth knowledge

--- face2.py
+++ face2.py
@@ -26,19 +26,27 @@ video_capture=cv2.VideoCapture(0)
 while True:
     #Capture Frame by Frame
     ret,frame=video_capture.read()
+    # ret=true if reade frame sucessfully and frame has a numpy array of frome data
     # find all face locations in current frame
     face_locations=face_recognition.face_locations(frame)
+    # detect all the faces in fram and give tupple codinate top right left bottom
     face_encodings=face_recognition.face_encodings(frame,face_locations)
+    # convert face into 128 dimensional array used to compare faces
 
     # loop through each face found in frame
     for (top,right,bottom,left), face_encoding in zip(face_locations,face_encodings):
+        # zip pair each face wit its encoding
         #  check if face match any known face
         matches=face_recognition.compare_faces(known_face_encodings,face_encoding)
+        # ccomapare features
         name="Unknown"
+        # if face is not recognised by deafault value
 
         if True in matches:
             first_match_index=matches.index(True)
+            # gives index of matched face
             name=known_face_name[first_match_index]
+            # name of matched face
         
         # Draw a box around the face and label the name
         cv2.rectangle(frame, (left,top),(right,bottom),(0,0,255),2)
@@ -49,6 +57,7 @@ while True:
 
     # break the loop when the 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        # & oxFF checks compatibilty with different system
         break
 
 #  release webcam and close opencv windows
